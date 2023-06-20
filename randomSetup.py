db7d1eaf6de48658b860ea454d68dce9b8ccd238
Domenico Commisso <dcommisso@users.noreply.github.com>
2023-06-20 19:53:56 +0200

    Refactor to object-oriented
    and add power to choose allies

--- randomSetup.py
+++ randomSetup.py
@@ -2,67 +2,87 @@
 
 from random import shuffle
 
-suits = ["artigli",
-        "fiori",
-        "occhi",
-        "foglie"]
 
-royal_titles = ["Fante",
-          "Regina",
-          "Re",
-          "Principe",
-          "Marchesa",
-          "Barone"]
+class RulersDeck:
+    """A deck of For Northwood! ruler cards"""
+
+    def __init__(self):
+
+        self.undrawn_cards = {}
+        self.allies = []
+        self.neutrals = []
+
+        suits = ["artigli",
+                 "fiori",
+                 "occhi",
+                 "foglie"]
+
+        royal_titles = ["fante",
+                        "regina",
+                        "re",
+                        "principe",
+                        "marchesa",
+                        "barone"]
+
+        for suit in suits:
+            self.undrawn_cards[suit] = []
+            for title in royal_titles:
+                self.undrawn_cards[suit].append(title + " di " + suit)
+
+    def __shuffle_by_suits(self):
+        """Shuffle each suit separately"""
+
+        for suit in self.undrawn_cards.keys():
+            shuffle(self.undrawn_cards[suit])
+
+    def __remove_card_from_deck(self, card):
+        suit_of_card_to_remove = card.split()[2].lower()
+        self.undrawn_cards[suit_of_card_to_remove].remove(card.lower())
+
+    def draw_neutral_rulers(self):
+        self.__shuffle_by_suits()
+        for suit in self.undrawn_cards:
+            for _ in range(2):
+                self.neutrals.append(self.undrawn_cards[suit][0])
+                self.__remove_card_from_deck(self.undrawn_cards[suit][0])
+        shuffle(self.neutrals)
+
+    def draw_allies(self, allies_to_draw=None):
+        """Draw specified allies from the deck or 4 random allies if no card
+        has been specified"""
+
+        if allies_to_draw:
+            for ally in allies_to_draw:
+                self.allies.append(ally.lower())
+                self.__remove_card_from_deck(ally)
+        else:
+            self.__shuffle_by_suits()
+            for suit in self.undrawn_cards:
+                self.allies.append(self.undrawn_cards[suit][0])
+                self.__remove_card_from_deck(self.undrawn_cards[suit][0])
+
+    def pretty_print_allies(self):
+        """Print allies in human-readable format"""
+
+        print("I tuoi alleati sono: " + ', '.join(self.allies).title())
+
+    def pretty_print_neutrals(self):
+        """Print neutrals in human-readable format"""
+
+        print("I sovrani neutrali sono:\n")
+        for fief, ruler in enumerate(self.neutrals):
+            print(f'Feudo {fief:d} -> {ruler:s}'.title())
 
 
-def rulers_init():
-    """This function initialize the rulers array"""
-
-    rulers = {}
-
-    for suit in suits:
-        rulers[suit] = []
-        for title in royal_titles:
-            rulers[suit].append(title + " di " + suit.capitalize())
-    return rulers
-
-
-def random_setup(rulers):
-    """Return 4 random allies and 8 random neutral rulers"""
-
-    allies = []
-    neutrals = []
-    for suit in rulers.keys():
-        shuffle(rulers[suit])
-        allies.append(rulers[suit][0])
-        neutrals.append(rulers[suit][1])
-        neutrals.append(rulers[suit][2])
-    shuffle(neutrals)
-    return allies, neutrals
-
-
-def pretty_print_allies(allies):
-    """Print allies in human-readable format"""
-    print("I tuoi alleati sono: " + ', '.join(allies))
-
-
-def pretty_print_neutrals(neutrals):
-    """Print neutrals in human-readable format"""
-
-    print("I sovrani neutrali sono:\n")
-    for fief, ruler in enumerate(neutrals):
-        print(f'Feudo {fief:d} -> {ruler:s}')
-
-
-def main():
-    """Generate a random For Northwood! game setup"""
-
-    rulers = rulers_init()
-    allies, neutrals = random_setup(rulers)
-    pretty_print_allies(allies)
-    print("--------")
-    pretty_print_neutrals(neutrals)
-
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     """Generate a random For Northwood! game setup"""
+#
+#     rulers = rulers_init()
+#     allies, neutrals = random_setup(rulers)
+#     pretty_print_allies(allies)
+#     print("--------")
+#     pretty_print_neutrals(neutrals)
+#
+#
+# if __name__ == "__main__":
+#     main()
